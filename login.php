5757f036d177ab8e701c8188d5c28ff81ffbfc4c
cholitosantika <127717925+cholitosantika@users.noreply.github.com>
2024-02-09 10:40:44 +0800

    Update login.php

--- login.php
+++ login.php
@@ -2,34 +2,38 @@
 ?>
 <!DOCTYPE html>
 <html>
-  <head>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <link rel="stylesheet" href="styles.css" />
-  </head>
-  <body>
+</head>
+<body>
     <div class="container">
-      <h1>Login</h1>
+        <header>
+            <h1>Login</h1>
+        </header>
 
-      <!-- Form Section -->
-      <form id="register" action="" method="post">
-        <input name="submitted" id="submitted" type="hidden" value="login" >
-        
-        <div class="form-group">
-          <label id="email-label" for="email">Email</label>
-          <input type="email" name="email" id="email" placeholder="Enter your Email" required>
-        </div>
+        <!-- Form Section -->
+        <form id="register" action="" method="post">
+            <input name="submitted" id="submitted" type="hidden" value="login" />
 
-        <div class="form-group">
-          <label id="password-label" for="password">Password</label>
-          <input type="password" name="password" id="password" placeholder="Enter your Password" required>
-        </div>
-
-        <div class="form-group">
-          <button type="submit" id="submit" class="submit-button">
-            Register
-          </button>
-        </div>
-      </form>
+            <div class="form-group">
+                <label id="username-label" for="username">Username</label>
+                <input type="text" name="username" id="username" placeholder="Enter your Usename" required />
+            </div>
+            <br />
+            <div class="form-group">
+                <label id="password-label" for="password">Password</label>
+                <input type="password" name="password" id="password" placeholder="Enter your Password" required />
+            </div>
+            <br />
+            <div class="form-group">
+                <button type="submit" id="submit" class="submit-login"> <h2>Login</h2>
+                </button>
+            </div>
+        </form>
+        <br />
+        <p>Don't have an account yet? <a href="registration.php">Create one</a></p>
     </div>
-  </body>
+</body>
 </html>
+
