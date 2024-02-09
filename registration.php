9f48a1b773f91d23336556704d3f214fd7c12f24
cholitosantika <127717925+cholitosantika@users.noreply.github.com>
2024-02-09 10:39:59 +0800

    Update registration.php

--- registration.php
+++ registration.php
@@ -8,32 +8,35 @@
 </head>
 <body>
     <div class="container">
-        <h1>Registration</h1>
+        <header>
+            <h1>Registration</h1>
+        </header>
 
         <!-- Form Section -->
         <form id="register" action="" method="post">
-          <input name="submitted" id="submitted" type="hidden" value="registration" >
-          
             <div class="form-group">
                 <label id="username-label" for="username">Username</label>
                 <input type="text" name="username" id="username" placeholder="Enter your Usename" required />
             </div>
-
+            <br />
             <div class="form-group">
                 <label id="email-label" for="email">Email</label>
                 <input type="email" name="email" id="email" placeholder="Enter your Email" required />
             </div>
-
+            <br />
             <div class="form-group">
                 <label id="password-label" for="password">Password</label>
                 <input type="password" name="password" id="password" placeholder="Enter your Password" required />
             </div>
-
+            <br />
             <div class="form-group">
-                <button type="submit" id="submit" class="submit-button"> Register
+                <button type="submit" id="submit" class="submit-register"><h2>Registration</h2> 
                 </button>
             </div>
         </form>
+        <br />
+        <p>Already have an account? <a href="login.php">Login here!</a></p>
     </div>
 </body>
 </html>
+
